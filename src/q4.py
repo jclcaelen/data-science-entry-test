e2030d99c4e8d8e613f2042e5d3208c58c5dc500
jclcaelen <37214087+jclcaelen@users.noreply.github.com>
2026-03-09 17:22:02 +0800

    Completed Task 1 and 2
    Added input validation and implemented string reversal logic, invoked examples.

--- src/q4.py
+++ src/q4.py
@@ -5,10 +5,25 @@ def string_reverse(s):
     - s must be a string.
     - Return the reversed string.
     """
-    return
+
+    # Check if s is a string
+    if not isinstance(s, str):
+        return -1
+
+
+    # Create a temp variable to extract and store the reversed, using a for loop
+    # For loop is as such where the last char is of n-1 index, and we stop at -1 to include 0th index, and -1 step: backwards
+    reversed_s = ""
+    for i in range(len(s)-1, -1, -1)
+        reversed_s += s[i]
+    
+    return reversed_s
 
 
 # Task 2
 # Invoke the function "string_reverse" using the following scenarios:
 # - "Hello World"
 # - "Python"
+
+string_reverse("Hello World")
+string_reverse("Python")
