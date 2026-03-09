30a57d38461a788f31068b8a50792096288142e5
jclcaelen <37214087+jclcaelen@users.noreply.github.com>
2026-03-09 17:45:03 +0800

    Completed Tasks 1 and 2
    Added the codes to initialise and the describe_car method.
    
    Invoked example to be called and then described.

--- src/q7.py
+++ src/q7.py
@@ -6,10 +6,20 @@ class Car:
     - Add a method named describe_car() that prints information about the car as "Year Make Model"
     """
 
+    # Example of creating new classes, and the structure:
+    # https://stackoverflow.com/questions/8609153/why-do-we-use-init-in-python-classes
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
     def describe_car(self):
-        print("")
+        print("f{self.year} {self.make} {self.model}")
 
 
 # Task 2
 # Create an instance of the Car class with the following attributes and call describe_car method:
 # - Make: Toyota, Model: Corolla, Year: 2020
+
+new_car = Car("Toyota", "Corolla", 2020)
+new_car.describe_car()
