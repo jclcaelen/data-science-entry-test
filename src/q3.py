7e8a27b050f03577bef8714161e74bbbb4f29530
jclcaelen <37214087+jclcaelen@users.noreply.github.com>
2026-03-09 17:12:13 +0800

    Completed Task 1 and 2
    Added checks for dictionary type and key existence before updating; invoked examples.

--- src/q3.py
+++ src/q3.py
@@ -5,10 +5,29 @@ def update_dictionary(dct, key, value):
     - If the key already exists in dct, print the original value, then update its value.
     - Return the updated dictionary.
     """
-    return
+
+    # Though not requested, will apply the same check that the dictionary is given to check
+    if not isinstance(dct, dict):
+        return -1
+
+    # Check if key exists
+    if key not in dct:
+        print(f"Error: key '{key}' does not exist in the dictionary")
+        return -1
+
+
+    # Update the key with new value
+    dct[key] = value
+
+
+    # Print the updated dictionary
+    print("Updated dictionary: ", dct)
 
 
 # Task 2
 # Invoke the function "update_dictionary" using the following scenarios:
 # - {}, "name", "Alice"
 # - {"age": 25}, "age", 26
+
+update_dictionary({}, "name", "Alice")
+update_dictionary({"age": 25}, "age", 26)
