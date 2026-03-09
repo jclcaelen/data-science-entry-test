c4564431a389b866e6dda50b1e2962fa845a0392
jclcaelen <37214087+jclcaelen@users.noreply.github.com>
2026-03-09 17:29:15 +0800

    Completed Tasks 1 and 2
    Implement check_divisibility function with error handling for non-numeric types and division by zero, and invoked examples

--- src/q5.py
+++ src/q5.py
@@ -5,10 +5,30 @@ def check_divisibility(num, divisor):
     - Both num and divisor must be numeric.
     - Return True if num is divisible by divisor, False otherwise.
     """
-    return
+
+    # Unlike q1.py, where we just check if the given variable can be typecast to float, we use isinstance here to check if it is an integer or float.
+    # https://stackoverflow.com/questions/33311258/python-check-if-variable-isinstance-of-any-type-in-list, shows we can use a tuple for multiple types
+    if not (isinstance(num, (int, float)) and isinstance(divisor, (int, float))):
+        return -1
+
+
+    # Check if divisible, return true if yes, and false if no. we use modular %, where 0 means divisible.
+    # However, if divisor is given as 0, we need to flag the error
+    if divisor == 0:
+        print("Error: Division by 0")
+        return -1
+
+    if num % divisor == 0:
+        return True
+    else
+        return False
 
 
 # Task 2
 # Invoke the function "check_divisibility" using the following scenarios:
 # - 10, 2
 # - 7, 3
+
+check_divisibility(10, 2)
+check_divisibility(7, 3)
+
